ea1478905611e81c43a131e860e2c08e99243c6a
Liam Nelski <ljnelski@gmail.com>
2019-10-30 14:11:55 -0400

    [Add] Asteroids, collision improvements

---
+++ Assets/Scripts/RandomRotater.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomRotater : MonoBehaviour
+{
+    private Rigidbody2D rBody;
+
+    public float tumble;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rBody = GetComponent<Rigidbody2D>();
+        rBody.angularVelocity = Random.value * tumble;
+    }
+
+    
+}
